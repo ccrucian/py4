d2a0fecf2646b8233d78cda06656df59514bd934
ccrucian <carlotta.cruciani1@gmail.com>
2026-09-20 12:36:46 +0200

    ok

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,36 @@
+import sys
+import typing
+
+
+def take_args(args: list[str]) -> None:
+    if len(args) == 1:
+        print("Usage:  ft_ancient_text.py <file>\n")
+        return
+    else:
+        print("=== Cyber Archives Recovery === ")
+        name: str = args[1]
+        print(f"Accessing file: '{name}'")
+    try:
+        fd: typing.IO[str] = open(name, "r")
+        try:
+            text = fd.read()
+            print("---\n")
+            print(f"{text}")
+            print("\n---")
+        finally:
+            fd.close()
+            print(f"File '{name}' closed.")
+    except FileNotFoundError as e:
+            print(f"Error opening file {name}: {e}")
+    except PermissionError as e:
+            print(f"Error opening file {name}: {e}")
+    except Exception as e:
+        print(f"{e}")
+        
+
+def main() -> None:
+    take_args(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
